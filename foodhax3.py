00443f73458b368a7f255a653f4b89cc880e65d8
Christina Milner <christina@christina-milner.dev>
2023-10-17 12:22:42 +0100

    All fixed, Friday default deprecated for now

--- foodhax3.py
+++ foodhax3.py
@@ -25,59 +25,6 @@ weekFull = ['Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday', 
 weekShort = ['tue', 'wed', 'thu', 'fri', 'sat', 'sun', 'mon']
 days = []
 
-for i in range(len(weekFull)):
-    days.append(Day(weekFull[i], weekShort[i]))
-
-
-def loadDishes():
-    cnt = 0
-    fhand = open('food.csv')
-    for line in fhand:
-        cnt += 1
-        line = line.rstrip()
-        line = line.split(',')
-        stuff = [x for x in line[3].split(';')]
-        dishes.append(Dish(cnt, line[0], line[1], line[2], stuff))
-    fhand.close()
-
-
-## Main program loop
-
-print('Welcome to Christina\'s food hax!')
-loadDishes()
-print('There are {} dishes to choose from.\n'.format(len(dishes)))
-# TODO: 1. Finish this part (any day start and end) 2. Make week arrays less stupid (just use  one and add food info to each day) 3. Outsource printing of week schedule
-# to separate function so it can be called at any time while picking out the food 4. (Stretch) option to edit the existing dishes while still in the loop
-start = input('What day to start from? (mon, tue, wed, thu, fri, sat, sun - default Tuesday)')
-if not start or start not in weekShort:
-    start = 0
-end = input('What day to end at? (mon, tue, wed, thu, fri, sat, sun - default Monday)')
-if not end or end not in weekShort:
-    end = 6
-days = days[start:].append(days[0:start]) #Figure out how to make chosen end work
-
-fr = input('Kebabs on Friday as usual?\n\'Y\' for yes or anything else for no.\n')
-if fr.lower() == 'y':
-    kebabs = next((item for item in dishes if item.name == "Kebabs"), None)
-        if dish.name == 'Kebabs':
-            #for x in dish.ings:
-                #ingrs[x] = ingrs.get(x, 0) + 1
-            days['Friday'] = dish  # Rethink the days array, need to be able to access them directly by name
-            week.remove('Friday')
-            dishes.remove(dish)
-
-""" if 'Saturday' not in days:
-    sat = input('Poutine on Saturday as usual?\n\'Y\' for yes or anything else for no.\n')
-    if sat.lower() == 'y':
-        for dish in dishes:
-            if dish.name == 'Poutine':
-                #for x in dish.ings:
-                    #ingrs[x] = ingrs.get(x, 0) + 1
-                days['Saturday'] = dish
-                week.remove('Saturday')
-                dishes.remove(dish) """
-# Poutine is cancelled for now
-
 def lushes(x):
     if x == 'r':
         print('You have chosen red wine.\n')
@@ -91,12 +38,26 @@ def lushes(x):
                 print(dish.number, dish.name, dish.wine)
 
 
-while week:
-    booze = None
-    dinner = None
-    day = week[0]
-    print('What do we want to eat on {}?\n'.format(day))
-    choice = input('1 for the full list of dishes\n2 to choose by wine type ')
+def loadDishes():
+    cnt = 0
+    fhand = open('food.csv')
+    for line in fhand:
+        cnt += 1
+        line = line.rstrip()
+        line = line.split(',')
+        stuff = [x for x in line[3].split(';')]
+        dishes.append(Dish(cnt, line[0], line[1], line[2], stuff))
+    fhand.close()
+
+def printCurrent():
+    for day in days:
+        try:
+            print('{}: {}, {}'.format(day.fullName, day.dish.name, day.dish.wine))
+        except:
+            print('{}: No food info yet.'.format(day.fullName))
+
+def getChoice():
+    choice = input('1 for the full list of dishes\n2 to choose by wine type\n3 to see the current list\n')
     if choice == '1':
         for dish in dishes:
             print(dish.number, dish.name)
@@ -109,28 +70,80 @@ while week:
             lushes('r')
         else:
             print('That\'s not a valid wine.')
+    if choice == '3':
+        printCurrent()
+        getChoice()
+
+## Main program loop
+
+print('Welcome to Christina\'s food hax!')
+loadDishes()
+print('There are {} dishes to choose from.\n'.format(len(dishes)))
+start = input('What day to start from? (mon, tue, wed, thu, fri, sat, sun - default Tuesday)\n')
+num_days = input('How many days?\nDefault 7.')
+if num_days:
+    try:
+        num_days = int(num_days)
+    except:
+        num_days = 7
+else:
+    num_days = 7
+if start.lower() in weekShort:
+    start = weekShort.index(start.lower())
+else:
+    start = 0
+
+end = start + num_days
+
+while start < end:
+    days.append(Day(weekFull[start % len(weekFull)], weekShort[start % len(weekShort)]))
+    start += 1
+
+
+for day in days:
+    dinner = None
+    booze = None
+    print('What do we want to eat on {}?\n'.format(day.fullName))
+    getChoice()
     while dinner is None:
-        ask = input('Enter your meal choice for {} by its number.\n'.format(day))
+        ask = input('Enter your meal choice for {} by its number.\n'.format(day.fullName))
         try:
             dinner = int(ask)
             for dish in dishes:
                 if dish.number == dinner:
-                    days[day] = dish
+                    day.dish = dish
                     #for x in dish.ings:
                         #ingrs[x] = ingrs.get(x, 0) + 1
-                    week.remove(day)
                     if not 'Other' in dish.name:
                         dishes.remove(dish)
         except:
             print('That doesn\'t seem to be a valid number.')
+printCurrent()
 
-#print(days)
-for day in week2:
-    food = days[day]
-    print('{}: {}, {}'.format(day, food.name, food.wine))
+
 """ q = input('\nWould you like to see the list of ingredients required?\n')
 if q.lower() == 'y' or q.lower() == 'yes':
     print('Ingredients required:')
     for (ing, amount) in ingrs.items():
         print(ing, 'x', amount)
  """
+""" fr = input('Kebabs on Friday as usual?\n\'Y\' for yes or anything else for no.\n')
+if fr.lower() == 'y':
+    kebabs = next((item for item in dishes if item.name == "Kebabs"), None)
+        if dish.name == 'Kebabs':
+            #for x in dish.ings:
+                #ingrs[x] = ingrs.get(x, 0) + 1
+            days['Friday'] = dish  # Rethink the days array, need to be able to access them directly by name
+            week.remove('Friday')
+            dishes.remove(dish) """
+
+""" if 'Saturday' not in days:
+    sat = input('Poutine on Saturday as usual?\n\'Y\' for yes or anything else for no.\n')
+    if sat.lower() == 'y':
+        for dish in dishes:
+            if dish.name == 'Poutine':
+                #for x in dish.ings:
+                    #ingrs[x] = ingrs.get(x, 0) + 1
+                days['Saturday'] = dish
+                week.remove('Saturday')
+                dishes.remove(dish) """
